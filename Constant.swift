217af0728822fdea23dd759fc80062c74b9ce5c7
Ajay <ajay.kumar@iphtechnologies.com>
2024-01-12 11:56:24 +0530

    live capture camera added

---
+++ Constant.swift
@@ -0,0 +1,37 @@
+//
+//  Constant.swift
+//  VisionHandPose
+//
+//  Created by iPHTech 28 on 11/01/24.
+//
+
+import Foundation
+
+struct Constant{
+    
+    
+    static let useThumb = "use your thumb"
+    static let cameraFeedData = "CameraFeedDataOutput"
+    static let nope = "Nope"
+    static let like = "Like"
+    static let undo = "undo"
+    static let noFrontCamera = "Could not find a front facing camera."
+    static let  notCreateVideo =  "Could not create video device input."
+    static let notAddVideoDevice = "Could not add video device input to the session"
+    static let notAddVideoData = "Could not add video data output to the session"
+    
+    
+    struct ImageName{
+        
+        static let image1 = "image1"
+        static let image2 = "image2"
+        static let image3 = "image3"
+        static let image4 = "image4"
+        static let image5 = "image5"
+        static let image6 = "image6"
+        static let image7 = "image7"
+        static let image8 = "image8"
+        static let image9 = "image9"
+        
+    }
+}
